cf19f4c0227f64a67a24b20f0a51194b352d1d49
James McCabe <jpmccabe@mit.edu>
2013-12-19 16:01:37 -0500

    enum for languang operations added.

---
+++ src/model/Operation.java
@@ -0,0 +1,7 @@
+package model;
+
+// Represents all of the operation in the language.
+public enum Operation {
+    RIGHT_SHIFT_POINTER, LEFT_SHIFT_POINTER, INCREMENT_POINTER,
+    DECREMENT_POINTER, PRINT, LOOP_BEGIN, LOOP_END;
+}
